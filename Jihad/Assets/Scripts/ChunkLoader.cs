b6e7dff9b6ba8128207d768470ba0823d0ced9ba
Raphael Lavoie <rapolysec@hotmail.com>
2018-11-25 10:11:21 -0500

    Meilleur ChunkLoader avec support de container

--- Jihad/Assets/Scripts/ChunkLoader.cs
+++ Jihad/Assets/Scripts/ChunkLoader.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ChunkLoader : MonoBehaviour {
 
     public GameObject[] UsableChunkList;
+    public GameObject Container;
     private float[,] ChunkPositionList;
     
     
@@ -19,37 +20,86 @@ public class ChunkLoader : MonoBehaviour {
             for (int k = 0; k < 8; k++)
             {
                 int t_RandomChunkNumber = Random.Range(0, UsableChunkList.Length);
+                GameObject t_NewChunk;
                 if (ChunkPositionList[i, k] != 0)
                 {
                     continue;
                 }
                 if (i == 0 && k == 0)
                 {
-                    Instantiate(UsableChunkList[0], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.identity);
+                    t_NewChunk = Instantiate(UsableChunkList[0], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.identity) as GameObject;
+                    t_NewChunk.transform.parent = Container.transform;
                     ChunkPositionList[i, k] = 1;
                     Debug.Log(ChunkPositionList[0, 0]);
                     continue;
                 }
+                if (k > 0)
+                {
+                    //Si le chunk en dessous est une intersection, on génère une route normale
+                    if (ChunkPositionList[i, k - 1] == 1)
+                    {
+                        t_NewChunk = Instantiate(UsableChunkList[1], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.identity) as GameObject;
+                        t_NewChunk.transform.parent = Container.transform;
+
+                    }
+                    //Si le chunk en dessous est une route horizontale, on ne peut pas générer ni d'intersection, ni de route horizontale
+                    if (ChunkPositionList[i, k - 1] == 2.5)
+                    {
+                        while (t_RandomChunkNumber == 0)
+                        {
+                            t_RandomChunkNumber = Random.Range(0, UsableChunkList.Length);
+                        }
+                        if (t_RandomChunkNumber == 2)
+                        {
+                            t_NewChunk = Instantiate(UsableChunkList[1], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.Euler(0, 0, 90)) as GameObject;
+                            t_NewChunk.transform.parent = Container.transform;
+                            ChunkPositionList[i, k] = 2.5f;
+                            continue;
+                        }
+
+                    }
+                }
                 if (i > 0)
                 {
+                    //Si le chunk à gauche est une intersection, les chunks de route seront tourné de 90 degrés pour y être connecter
                     if (ChunkPositionList[i - 1, k] == 1)
                     {                       
                         if (t_RandomChunkNumber == 1)
                         {
-                            Instantiate(UsableChunkList[1], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.Euler(0, 0, 90));
+                            t_NewChunk = Instantiate(UsableChunkList[1], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.Euler(0, 0, 90)) as GameObject;                         
+                            t_NewChunk.transform.parent = Container.transform;
                             ChunkPositionList[i, k] = 2.5f;
                             continue;
                         }
                     
                     }
+                    //Si le chunk à gauche est une route allant vers le haut, on génère un chunk de batiment sur son long
+                    if (ChunkPositionList[i - 1, k] == 2)
+                    {
+                        t_NewChunk = Instantiate(UsableChunkList[2], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.Euler(0, 0, 90)) as GameObject;
+                        t_NewChunk.transform.parent = Container.transform;
+                        ChunkPositionList[i, k] = 3;
+                        continue;
+                    }
+                    //Si le chunk à gauche est une route à l'horizontale, on ne peut pas avoir de route verticale
+                    if (ChunkPositionList[i - 1, k] == 2.5)
+                    {
+                        if (t_RandomChunkNumber == 2)
+                        {
+                            t_NewChunk = Instantiate(UsableChunkList[1], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.Euler(0, 0, 90)) as GameObject;
+                            t_NewChunk.transform.parent = Container.transform;
+                            ChunkPositionList[i, k] = 2.5f;
+                            continue;
+                        }
+                        
+                    }
+                    
+
                 }
                 
-
-                    
-                
-
-                Instantiate(UsableChunkList[t_RandomChunkNumber], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.identity);
-                ChunkPositionList[i, k] = 1;
+                t_NewChunk = Instantiate(UsableChunkList[t_RandomChunkNumber], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.identity) as GameObject;
+                t_NewChunk.transform.parent = Container.transform;
+                ChunkPositionList[i, k] = t_RandomChunkNumber + 1;
             }
         }
 
