2a4a9207119a42ae3c16d1bd16054824c4a14c71
Raphael Lavoie <rapolysec@hotmail.com>
2018-11-24 21:11:29 -0500

    Chunk Loader fonctionnel

--- Jihad/Assets/Scripts/ChunkLoader.cs
+++ Jihad/Assets/Scripts/ChunkLoader.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class ChunkLoader : MonoBehaviour {
 
-    public GameObject[] ChunkList;
+    public GameObject[] UsableChunkList;
+    private float[,] ChunkPositionList;
     
     
     
     // Use this for initialization
 	void Start () {
 
-        
+        ChunkPositionList = new float[8, 8];
+        Debug.Log(ChunkPositionList[0, 0]);
         for (int i = 0; i < 8; i++)
         {
             for (int k = 0; k < 8; k++)
             {
-               Instantiate(ChunkList[Random.Range(0, 2)], new Vector3((i*10) - 35 , (k * 10) - 35, 0),  Quaternion.identity);
+                int t_RandomChunkNumber = Random.Range(0, UsableChunkList.Length);
+                if (ChunkPositionList[i, k] != 0)
+                {
+                    continue;
+                }
+                if (i == 0 && k == 0)
+                {
+                    Instantiate(UsableChunkList[0], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.identity);
+                    ChunkPositionList[i, k] = 1;
+                    Debug.Log(ChunkPositionList[0, 0]);
+                    continue;
+                }
+                if (i > 0)
+                {
+                    if (ChunkPositionList[i - 1, k] == 1)
+                    {                       
+                        if (t_RandomChunkNumber == 1)
+                        {
+                            Instantiate(UsableChunkList[1], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.Euler(0, 0, 90));
+                            ChunkPositionList[i, k] = 2.5f;
+                            continue;
+                        }
+                    
+                    }
+                }
                 
+
+                    
+                
+
+                Instantiate(UsableChunkList[t_RandomChunkNumber], new Vector3((i * 10) - 35, (k * 10) - 35, 0), Quaternion.identity);
+                ChunkPositionList[i, k] = 1;
             }
         }
 
